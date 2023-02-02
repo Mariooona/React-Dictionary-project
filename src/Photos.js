0509dfe79d1f2558c00ab70b178e38c49c927972
Mariooona <101356811+Mariooona@users.noreply.github.com>
2023-02-02 20:08:03 +0200

    fixing mistake

--- src/Photos.js
+++ src/Photos.js
@@ -9,13 +9,12 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a
-                  href={photo.src.original}
-                  alt="A picture of paradise"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={photo.src.landscape} className="img-fluid" />
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    alt="A picture of paradise"
+                    className="img-fluid"
+                  />
                 </a>{" "}
               </div>
             );
